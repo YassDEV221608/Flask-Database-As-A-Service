bc2ea5f4f3c2d6e6f89b87e07045cc6ad6be1056
Instagram Support <instagram.supp.ma@gmail.com>
2025-01-22 02:37:28 +0100

    Initial

--- web/app.py
+++ web/app.py
@@ -1,156 +1,122 @@
 from flask import Flask,jsonify,request
 from flask_restful import Api,Resource
 from pymongo import MongoClient
+import bcrypt
 
 app = Flask(__name__)
 api = Api(app)
 
 client = MongoClient("mongodb://db:27017")
-db = client.aNewDB
-UserNum = db["UserNum"]
+db = client.SentencesDatabase
+users = db["Users"]
 
-UserNum.insert_one({
-    'num_of_users': 0
-})
 
-class Visit(Resource):
-    def get(self):
-        prev_num = UserNum.find({})[0]["num_of_users"]
-        new_num = prev_num + 1
-        UserNum.update_one({},{"$set":{"num_of_users":new_num}})
+class Register(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData["password"]
+
+        hashed = bcrypt.hashpw(password.encode("utf8"),bcrypt.gensalt())
+
+        users.insert_one({
+            "Username": username,
+            "Password": hashed,
+            "Sentence": "",
+            "Token": 6
+        })
+
         resp = {
-            "Message": f"You are the user no : {new_num}",
+            "Message": "You have been registered",
             "Status Code": 200
         }
-        return resp
+
+        return jsonify(resp)
+    
+def verify_pass(usrename,password):
+    hashed_pass = users.find({
+        "Username": usrename,
+    })[0]["Password"]
+    return bcrypt.hashpw(password.encode("utf8"),hashed_pass) == hashed_pass
 
 
-def checkData(postedData,functionName):
-    if functionName in ["add","substract","multiply","devide"]:
-        if "x" not in postedData or "y" not in postedData:
-            return 301
-        elif functionName == "devide" and postedData["y"] == 0:
-            return 302
-        else:
-            return 200
+def countTokens(username):
+    tokens = users.find({
+        "Username": username
+    })[0]["Token"]
+    return tokens
+
+
+class Store(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData["password"]
+        sentence = postedData["sentence"]
+
+        verify = verify_pass(username,password)
+
+        if not verify:
+            resp = {
+                "Status Code": 302
+            }
+            return jsonify(resp)
         
-        
+        num_token = countTokens(username)
 
-def returnData(postedData,functionName,status_code):
-    res = 0
-    if "x" in postedData and "y" in postedData:
-        if functionName == "add":
-            res = postedData["x"] + postedData["y"]
-        elif functionName == "substract":
-            res = postedData["x"] - postedData["y"]
-        elif functionName == "multiply":
-            res = postedData["x"] * postedData["y"]
-        elif functionName == "devide":
-            if postedData["y"] == 0:
-                return False
-            res = postedData["x"] / postedData["y"]
+        if num_token <= 0:
+            resp = {
+                "Status Code": 301
+            }
+            return jsonify(resp)
+        
+        users.update_one({"Username": username},{"$set":{"Sentence":sentence,"Token":num_token-1}})
+
         resp = {
-            "Sum": res,
-            "Status Code": status_code
+                "Message": "Sentence saved successfuly",
+                "Status Code": 200
+            }
+        
+        return jsonify(resp)
+
+
+def getSentence(usermane):
+    sentence = users.find({
+        "Username": usermane
+    })[0]["Sentence"]
+    return sentence
+
+class Retreive(Resource):
+    def post(self):
+        postedData = request.get_json()
+
+        username = postedData["username"]
+        password = postedData["password"]
+
+
+        verify = verify_pass(username,password)
+
+        if not verify:
+            resp = {
+                "Status Code": 302
+            }
+            return jsonify(resp)
+        
+        sentence = getSentence(username)
+
+        resp = {
+            "Your Sentence": sentence
         }
-        return resp
-    else:
-        return False
+
+        return jsonify(sentence)
+        
 
 
-
-class Add(Resource):
-    def post(self):
-        postedData = request.get_json()
-
-        status_code = checkData(postedData,"add")
-
-        if status_code != 200:
-            resp = {
-                "Message": "An error occured",
-                "Status Code": status_code
-            }
-            return jsonify(resp)
-
-        resp = returnData(postedData,"add",status_code)
-
-        return jsonify(resp)
-    
-class Substract(Resource):
-    def post(self):
-        postedData = request.get_json()
-
-        status_code = checkData(postedData,"substract")
-
-        if status_code != 200:
-            resp = {
-                "Message": "An error occured",
-                "Status Code": status_code
-            }
-            return jsonify(resp)
-
-        resp = returnData(postedData,"substract",status_code)
-
-        return jsonify(resp)
-    
-class Multiply(Resource):
-    def post(self):
-        postedData = request.get_json()
-
-        status_code = checkData(postedData,"multiply")
-
-        if status_code != 200:
-            resp = {
-                "Message": "An error occured",
-                "Status Code": status_code
-            }
-            return jsonify(resp)
-
-        resp = returnData(postedData,"multiply",status_code)
-
-        return jsonify(resp)
-    
-class Devide(Resource):
-    def post(self):
-        postedData = request.get_json()
-
-        status_code = checkData(postedData,"devide")
-
-        if status_code != 200:
-            resp = {
-                "Message": "An error occured",
-                "Status Code": status_code
-            }
-            return jsonify(resp)
-
-        resp = returnData(postedData,"devide",status_code)
-
-        return jsonify(resp)   
-
-
-
-api.add_resource(Add,"/add")
-api.add_resource(Substract,"/substract")
-api.add_resource(Multiply,"/multiply")
-api.add_resource(Devide,"/devide")
-api.add_resource(Visit,"/visit")
-
-
-@app.route("/")
-def hello_world():
-    response = {
-        "yassine" : "laamiri"
-    }
-    return jsonify(response)
-
-
-@app.route("/sum",methods=["POST"])
-def sum_nums():
-    req = request.get_json()
-    x = req["x"]
-    y = req["y"]
-    res = {"results" : x+y}
-    return jsonify(res),200
+api.add_resource(Register,"/register")
+api.add_resource(Store,"/store")
+api.add_resource(Retreive,"/retreive")
 
 
 if __name__ == "__main__":
